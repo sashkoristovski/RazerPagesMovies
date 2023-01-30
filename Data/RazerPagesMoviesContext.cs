701700615cb3362f01163e89bb15e63f2d8bf62c
Sashko Ristovski <sashko.ristovski@yahoo.co.uk>
2023-01-30 11:51:30 +0000

    updated version

---
+++ Data/RazerPagesMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazerPagesMovies.Models;
+
+namespace RazerPagesMovies.Data
+{
+    public class RazerPagesMoviesContext : DbContext
+    {
+        public RazerPagesMoviesContext (DbContextOptions<RazerPagesMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazerPagesMovies.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazerPagesMovies.Data;
+using RazerPagesMovies.Models;
+
+namespace RazerPagesMovies.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazerPagesMovies.Data.RazerPagesMoviesContext _context;
+
+        public CreateModel(RazerPagesMovies.Data.RazerPagesMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
